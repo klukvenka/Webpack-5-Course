9ab9eb27829029a06b026c6700e7d81b17240383
klukvenka <aida_g888@mail.ru>
2022-07-29 10:53:18 +0200

    Small fix

--- 4. SCSS Loaders/webpack.config.js
+++ 4. SCSS Loaders/webpack.config.js
@@ -14,6 +14,9 @@ module.exports = {
           // all the loaders that are used for loading for that css,
           { loader: 'style-loader' },
           { loader: 'css-loader', options: { modules: true } },
+          // THE LOADERS ORDER IS IMPORTANT, the executions is from RIGHT TO LEFT
+          // 1. css loader sees index.css imported --> he doesn't know what to do with it --> passes it to style-loader
+          // 2. style-loader takes what WAS PROCESSED by css-loader and takes it to the html document
         ],
       },
       {
