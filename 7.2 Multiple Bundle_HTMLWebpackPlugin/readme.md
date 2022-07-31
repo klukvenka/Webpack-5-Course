a59fbd1a1ffdc7e71089ae36823d0991d5c4f991
klukvenka <aida_g888@mail.ru>
2022-08-01 00:31:19 +0200

    7.2 HTML webpack plugin

---
+++ 7.2 Multiple Bundle_HTMLWebpackPlugin/readme.md
@@ -0,0 +1,10 @@
+`Note: the main focus is configuring a HTMLWebpack plugin`
+
+The website is an example of a multiple page website that is used to sell such products as cakes and so on.
+
+HTML Webpack Plugin injects all the dependencies directly into the html file (adding the bundle file in html).
+
+1. run `npm run build` to compile the code with webpack
+2. Open **dist/index.html** to see the page
+
+In this example the assets and index.css were copied manually into the dist/ directory. The next section is going to intoduce automated webpack
---
+++ 7.2 Multiple Bundle_HTMLWebpackPlugin/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin'); // importing the plugin
+
+module.exports = {
+  entry: {
+    index: './src/index.js',
+    product: './src/products.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // to use plugins:
+  // When configuration inside htmlWebpackPlugin({}) is empty it's going to generate an html file
+  // We can add some configurations in order to tell the plugin to use our html files (that have other code in them that we need) instead of generating a new one
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'), // template that is going to be used
+      chunks: ['index'], // dependencies of the particulate template, in our case index.html file depends on the index entry (index.js)
+      inject: true, // inject the deoendency in the template
+      filename: 'index.html',
+    }),
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/products.html'),
+      chunks: ['product'],
+      inject: true,
+      filename: 'product.html', // have to be specified when we have 2 of these plugins
+    }),
+  ],
+};
